9b454d8abaa981b7cbc47f43e2fcb33534cc3d05
Passapol <110749961+passapol146990@users.noreply.github.com>
2023-06-16 20:34:50 +0700

    cm3

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = require('./router/myrouter')
 
 app.listen(4000,()=>{
     console.log('listening on')
 })
 
-app.get('/',(req,res)=>{
-    res.send('Hello, world')
-})
-
-app.get('/about',(req,res)=>{
-    res.send('about')
-});
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine','ejs')
+app.use(router)
 
 module.exports = app
---
+++ router/myrouter.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { route, router } = require('..')
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    const name = 'passapol';
+    const age = 18
+    const address = "<h2>กาฬสินธุ์</h2>"
+    res.render('index.ejs',{name:name,age:age,address:address});
+})
